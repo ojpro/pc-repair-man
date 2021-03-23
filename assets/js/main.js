09d5c432fca7db00ad82bbc4a5b0b9c254d49ab5
ojpro <eljabbarioussama.mail@gmail.com>
2021-03-23 19:30:10 +0100

    Remove Statistics Section , Unneed Images

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,19 +27,9 @@ testimonials.on('slideChange', function () {
 })
 overlay.style.backgroundColor = swiperColors[Math.floor(Math.random(10) * swiperColors.length)];
 
-// Google Map API
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+// FAQs Accordion
+
+contactFields = document.querySelector('input[type="text"],input[type="email"],textarea')
+contactFields.onchange = function(){
+  this.classList.add('changed')
 }
